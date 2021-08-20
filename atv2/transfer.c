28196a2de278f9126c5e5496a091df80658aa23d
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2021-08-20 13:07:35 +0000

    save

--- atv2/transfer.c
+++ atv2/transfer.c
@@ -1,33 +1,66 @@
 #include <stdio.h>
 #include <math.h>
 
+float a(double t, int r, int k);
+float b(double t, int r, int p, int k);
+float c(double t, int r, int p, int k);
+
 int main() {
-    double temp_total; /* s */
-    double t = 1; /* tamanho, MB 10^6 * 8 */
-    int r = 100; /* RTT, ms */
+    double tempo_total; /* segundos */
+    double t; /* tamanho, MB 10^6 * 8 */
+    int r; /* RTT, ms */
     int p; /* KB */
-    int k = 2; /* k x RTT */
-    double l; /* largura banda, Mbps */
-    int q; /* pacotes */
-    double handshake = k*r*pow(10, -3);
+    int k; /* k x RTT */
+    char s; /* situação A, B, C ou D */
 
-    /*
-    t  r  p k s
+    scanf("%lf %d %d %d %c", &t, &r, &p, &k, &s);
 
-    1 100 1 2 A
-    
-    l
-    1.5
-    */
-    /*printf("ms to s: %f\n", 100*pow(10, -3));
-    printf("MB to Mb %d\n", 1*8);
-    printf("Mb / Kb: %f\n", (1*8) / ((1*8)*pow(10, -3)));
-    printf("Mb / Mbps: %f\n", (1*8) / 1.5);
+    switch(s) {
+        case 'A':
+           tempo_total = a(t, r, k);
+           break;
+        case 'B':
+            tempo_total = b(t, r, p, k);
+            break;
+        case 'C':
+            tempo_total = b(t, r, p, k);
+            break;
+    }
 
-    printf("handshack: %f\n", handshake);*/
-
-    printf("Tempo de transmissão: %f\n", ( handshake + (r*pow(10, -3)) + ( t*8 / 1.5)));
-
+    printf("Tempo de transmissão: %f\n", tempo_total);
 
     return 0;
+}
+
+float a(double t, int r, int k) {
+    double l; /* largura de banda */
+    double rtt = r*pow(10, -3);
+    double handshake = k*rtt;
+
+    scanf("%lf", &l);
+
+    
+    return handshake + (rtt/2) + (t*8 / l);
+}
+
+float b(double t, int r, int p, int k) {
+    double l;
+    double rtt = r*pow(10, -3);
+    double handshake = k*rtt;
+    int pacotes = t / (p*pow(10, -3));
+
+    scanf("%lf", &l);
+    
+    return handshake + (rtt/2) + (rtt*(pacotes-1)) + (t*8 / l);
+}
+
+float c(double t, int r, int p, int k) {
+    int q;
+    double rtt = r*pow(10, -3);
+    double handshake = k*rtt;
+    int pacotes = t / (p*pow(10, -3));
+
+    scanf("%d", &q);
+    
+    return handshake + pacotes;
 }
