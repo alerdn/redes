0b91f7ac1db42c648f1c037766d3c2ce87e2e3b3
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2021-08-31 12:42:59 +0000

    inicio manchester

--- atv3/cod_transmissor.c
+++ atv3/cod_transmissor.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *step1(char **cod);
+char *step1(char *cod, char *k);
 int *step2(char *msg);
 int *step3(int *msg2);
 
 void nrz(char *cod, int length, int *msg) {
   char arr[8];
 
-  printf("%s %d ", cod, length*8);
-  for (int i = 1; i < length+1; i++) {
+  printf("%s %d ", cod, length * 8);
+  for (int i = 1; i <= length; i++) {
     for (int j = 7; j >= 0; j--) {
       arr[j] = (msg[i] % 10) == 1 ? 'A' : 'B';
       msg[i] /= 10;
@@ -20,16 +20,38 @@ void nrz(char *cod, int length, int *msg) {
   }
 }
 
+void manchester(char *cod, char k, int length, int *msg) {
+  char arr[16];
+
+  printf("%s %c %d ", cod, k, length * 16);
+  for (int i = 1; i <= length; i++) {
+    for (int j = 15; j >= 0; j--) {
+
+      arr[j] = ((msg[i] % 10) == 1 ? 'A' : (k == 'A' ? 'A' : 'B'));
+      k = (k == 'A' ? 'B' : 'A');
+      //k = (k == 'A' ? 'B' : 'A');
+
+      msg[i] /= 10;
+      printf("%d\n", j);
+    }
+
+    printf("%s\n", arr);
+  }
+}
+
 int main() {
-  char *cod;
-  char *msg = step1(&cod);
+  char *cod = (char *)malloc(sizeof(char *) * 20);
+  char k;
+  char *msg = step1(cod, &k);
   int *msg2 = step2(msg);
   int *msg3 = step3(msg2);
 
-  printf("c: %s\n", cod);
+  printf("c: %s\nk: %c\n", cod, k);
 
   if (strcmp(cod, "NRZ") == 0) {
     nrz(cod, msg2[0], msg3);
+  } else if (strcmp(cod, "Manchester") == 0) {
+    manchester(cod, k, msg2[0], msg3);
   }
 
   printf("\n");
@@ -41,14 +63,17 @@ int main() {
   return 0;
 }
 
-char *step1(char **cod) {
+char *step1(char *cod, char *k) {
   char *msg;
-  char *c;
-  msg = (char *)malloc(sizeof(char) * 80);
-  c = (char *)malloc(sizeof(char *) * 20);
-  scanf("%s %[^\n]s", c, msg);
 
-  *cod = c;
+  msg = (char *)malloc(sizeof(char) * 80);
+  scanf("%s ", cod);
+
+  if (strcmp(cod, "Manchester") == 0) {
+    scanf("%c ", k);
+  }
+
+  scanf("%[^\n]s", msg);
 
   return msg;
 }
