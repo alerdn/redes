a241b46201de7080021e270e6e766b06756a60ec
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2021-09-02 03:04:24 +0000

    fim atv3

--- atv3/cod_receptor.c
+++ atv3/cod_receptor.c
@@ -3,38 +3,15 @@
 #include <string.h>
 
 char *step1(char *cod, char *k, int *len);
-int *step2(char *msg, int len);
+int *step2(char *msg, char k, int len);
 int *convert(int *msg, int len);
 int *step3(int *msg2);
 
-void nrz(char *cod, int length, int *msg) {
+void show(char *cod, int length, int *msg) {
   int *msgFinal = convert(msg, length);
 
   for (int i = 0; i < length; i++) {
     printf("%c", msgFinal[i]);
-  }
-}
-
-void manchester(char *cod, char k, int length, int *msg) {
-  char arr[16];
-
-  printf("%s %c %d ", cod, k, length * 16);
-  for (int i = 1; i <= length; i++) {
-    for (int j = 16; j > 0; j = j - 2) {
-
-      arr[j - 2] = ((msg[i] % 10) == 1 ? (k == 'A' ? 'B' : 'A')
-                                       : (k == 'A' ? 'A' : 'B'));
-      k = (k == 'A' ? 'B' : 'A');
-      arr[j - 1] = ((msg[i] % 10) == 1 ? (k == 'A' ? 'B' : 'A')
-                                       : (k == 'A' ? 'A' : 'B'));
-      k = (k == 'A' ? 'B' : 'A');
-
-      msg[i] /= 10;
-    }
-
-    for (int i = 0; i < 16; i++) {
-      printf("%c", arr[i]);
-    }
   }
 }
 
@@ -44,19 +21,9 @@ int main() {
   int len;
 
   char *msg = step1(cod, &k, &len);
-  int *msg2 = step2(msg, len);
+  int *msg2 = step2(msg, k, len);
 
-  if (strcmp(cod, "NRZ") == 0) {
-    nrz(cod, len, msg2);
-  }
-
-  /*if (strcmp(cod, "NRZ") == 0) {
-    nrz(cod, msg2[0], msg3);
-  } else if (strcmp(cod, "Manchester") == 0) {
-    manchester(cod, k, msg2[0], msg3);
-  }*/
-
-  printf("\n");
+  show(cod, len, msg2);
 
   free(cod);
   free(msg);
@@ -81,21 +48,39 @@ char *step1(char *cod, char *k, int *len) {
   return msg;
 }
 
-int *step2(char *msg, int len) {
+int *step2(char *msg, char k, int len) {
   int *msg2 = (int *)malloc(sizeof(int) * (len + 1));
   int num = 0;
   int d10;
+  int pos;
 
-  int i;
-  for (i = 0; i < len / 8; i++) {
-    num = 0;
-    d10 = 10000000;
-    for (int j = 0; j < 8; j++) {
-      num += msg[(i * 8) + j] == 'A' ? 1 * d10 : 0;
-      d10 /= 10;
+  // Quer dizer que é Manchester
+  if (k == 'A' || k == 'B') {
+    for (int i = 0; i < len / 16; i++) {
+      num = 0;
+      d10 = 10000000;
+      for (int j = 0; j < 16; j++) {
+        pos = (i * 16) + j;
+
+        if (pos % 2 == 0) {
+          num += (msg[pos] == 'A' ? (k == 'A' ? 0 : 1 * d10)
+                                  : (k == 'A' ? d10 : 0));
+          d10 /= 10;
+        }
+      }
+      printf("n: %d\n", num);
+      msg2[i] = num;
     }
-
-    msg2[i] = num;
+  } else {
+    for (int i = 0; i < len / 8; i++) {
+      num = 0;
+      d10 = 10000000;
+      for (int j = 0; j < 8; j++) {
+        num += msg[(i * 8) + j] == 'A' ? 1 * d10 : 0;
+        d10 /= 10;
+      }
+      msg2[i] = num;
+    }
   }
 
   return msg2;
@@ -125,24 +110,4 @@ int *convert(int *msg, int len) {
   }
 
   return msg;
-}
-
-int *step3(int *msg2) {
-  int length = msg2[0] + 1;
-  int *msg3 = (int *)malloc(sizeof(int) * length);
-
-  for (int i = 0; i < length; i++) {
-    int d = msg2[i];
-    int resp = 0;
-    int m10 = 1;
-    do {
-      resp += (d % 2) * m10;
-      m10 *= 10;
-      d /= 2;
-    } while (d != 0);
-
-    msg3[i] = resp;
-  }
-
-  return msg3;
 }
