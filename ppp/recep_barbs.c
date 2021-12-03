f4e57765d4d0965068902f71fdc3363615f9addf
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2021-12-03 02:35:31 +0000

    9,2 barbs

--- ppp/recep_barbs.c
+++ ppp/recep_barbs.c
@@ -1,297 +1,258 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define LEN_BIN 50000
-#define CARAC 30000
-#define WORDS 30
 
-int binTodec(int *bin){
-	int potenc = 1, i, decimal = 0;
-	
-	for(i=7; i>=0; i--){
-		decimal += bin[i]*potenc;
-		potenc = potenc * 2;
-	}
+int binToDec(int *bin, int len);
 
-	return decimal;
+void binToHex(int *prot, char *protocol);
+
+void concatenar(int *b1, int *b2, int *concat);
+
+int soma(int *b1, int *b2, int *resp, int vaiUm);
+
+int checksum_verify(int bytes_normalizados[30000][8], int *check_concat,
+                    int *prot, int campos, int data[30000][8], int index_data);
+
+int equal(int *b1, int *b2);
+
+void normalizar_input(int bytes_normalizados[30][6251][8], int *palavras,
+                      int input[30000][8], int qtd_bytes, int *campos);
+
+void print(int bytes_normalizados[30000][8], int campos, int index,
+           int palavras);
+
+int main() {
+  int len, bytes_normalizados[30][6251][8];
+  int bytes[30000][8];
+  int qtd_bytes, i, j, palavras = -1, campos[100];
+  char input[50000];
+
+  scanf("%s", input);
+  len = strlen(input);
+
+  qtd_bytes = len / 8;
+
+  for (i = 0; i < qtd_bytes; i++) {
+    for (j = 0; j < 8; j++) {
+      if (input[(i * 8) + j] == '0')
+        bytes[i][j] = 0;
+      else
+        bytes[i][j] = 1;
+    }
+  }
+
+  normalizar_input(bytes_normalizados, &palavras, bytes, qtd_bytes, campos);
+
+  for (i = 0; i < palavras; i++) {
+    print(bytes_normalizados[i], campos[i], i, palavras);
+  }
+
+  return 0;
 }
 
-void binTohexa(int *prot, char *protocol){
-	int i;
-	
-	for(i=0; i<4; i++){
-		if(prot[(i*4)] == 0 && prot[(i*4)+1] == 0 && prot[(i*4)+2] == 0 && prot[(i*4)+3] == 0)
-			protocol[i] = '0';
-		else if(prot[(i*4)] == 0 && prot[(i*4)+1] == 0 && prot[(i*4)+2] == 0 && prot[(i*4)+3] == 1)
-	    		protocol[i] = '1';
-		else if(prot[(i*4)] == 0 && prot[(i*4)+1] == 0 && prot[(i*4)+2] == 1 && prot[(i*4)+3] == 0)
-	    		protocol[i] = '2';
-		else if(prot[(i*4)] == 0 && prot[(i*4)+1] == 0 && prot[(i*4)+2] == 1 && prot[(i*4)+3] == 1)
-	    		protocol[i] = '3';
-		else if(prot[(i*4)] == 0 && prot[(i*4)+1] == 1 && prot[(i*4)+2] == 0 && prot[(i*4)+3] == 0)
-	    		protocol[i] = '4';
-		else if(prot[(i*4)] == 0 && prot[(i*4)+1] == 1 && prot[(i*4)+2] == 0 && prot[(i*4)+3] == 1)
-	    		protocol[i] = '5';
-		else if(prot[(i*4)] == 0 && prot[(i*4)+1] == 1 && prot[(i*4)+2] == 1 && prot[(i*4)+3] == 0)
-	    		protocol[i] = '6';
-		else if(prot[(i*4)] == 0 && prot[(i*4)+1] == 1 && prot[(i*4)+2] == 1 && prot[(i*4)+3] == 1)
-	    		protocol[i] = '7';
-		else if(prot[(i*4)] == 1 && prot[(i*4)+1] == 0 && prot[(i*4)+2] == 0 && prot[(i*4)+3] == 0)
-	    		protocol[i] = '8';
-		else if(prot[(i*4)] == 1 && prot[(i*4)+1] == 0 && prot[(i*4)+2] == 0 && prot[(i*4)+3] == 1)
-	    		protocol[i] = '9';
-		else if(prot[(i*4)] == 1 && prot[(i*4)+1] == 0 && prot[(i*4)+2] == 1 && prot[(i*4)+3] == 0)
-	    		protocol[i] = 'A';
-		else if(prot[(i*4)] == 1 && prot[(i*4)+1] == 0 && prot[(i*4)+2] == 1 && prot[(i*4)+3] == 1)
-	    		protocol[i] = 'B';
-		else if(prot[(i*4)] == 1 && prot[(i*4)+1] == 1 && prot[(i*4)+2] == 0 && prot[(i*4)+3] == 0)
-	    		protocol[i] = 'C';
-		else if(prot[(i*4)] == 1 && prot[(i*4)+1] == 1 && prot[(i*4)+2] == 0 && prot[(i*4)+3] == 1)
-	    		protocol[i] = 'D';
-		else if(prot[(i*4)] == 1 && prot[(i*4)+1] == 1 && prot[(i*4)+2] == 1 && prot[(i*4)+3] == 0)
-	    		protocol[i] = 'E';
-		else if(prot[(i*4)] == 1 && prot[(i*4)+1] == 1 && prot[(i*4)+2] == 1 && prot[(i*4)+3] == 1)
-	    		protocol[i] = 'F';
-	}
+int binToDec(int *bin, int len) {
+  int d2 = 1;
+  int dec = 0;
+
+  int i;
+  for (i = len - 1; i >= 0; i--) {
+    dec += bin[i] * d2;
+    d2 *= 2;
+  }
+
+  return dec;
 }
 
-void concatenation(int *vec1, int *vec2, int *concat){
-	int i;
-	
-	for(i=0; i<8; i++){
-		concat[i] = vec1[i];
-		concat[i+8] = vec2[i];
-	}
+void binToHex(int *prot, char *protocol) {
+  int hex = 0;
+  int dec = binToDec(prot, 16);
+
+  int i;
+  for (i = 3; i >= 0; i--) {
+    hex = dec % 16;
+    dec /= 16;
+
+    if (hex >= 10) {
+      protocol[i] = hex + 55;
+    } else {
+      protocol[i] = hex + 48;
+    }
+  }
 }
 
-int sum(int *vec1, int *vec2, int *resp){
-	int i, goOne=0;
-	
-	for(i=15; i>=0; i--){
-		if((vec1[i] == 0 && vec2[i] == 0)){
-			if(goOne == 1)
-				resp[i] = 1;
-			else
-				resp[i] = 0;	
-			goOne = 0;
-		}else if((vec1[i] == 1 && vec2[i] == 0) || (vec1[i] == 0 && vec2[i] == 1)){
-			if(goOne == 1){
-				resp[i] = 0;
-				goOne = 1;
-			}else{
-				resp[i] = 1;
-				goOne = 0;
-			}
-		}else if(vec1[i] == 1 && vec2[i] == 1){
-			if(goOne == 1){
-				resp[i] = 1;
-				goOne = 1;
-			}else{
-				resp[i] = 0;
-				goOne = 1;
-			}
-		}
-	}
-	return goOne;
+void concatenar(int *b1, int *b2, int *concat) {
+  int i;
+  for (i = 0; i < 8; i++) {
+    concat[i] = b1[i];
+    concat[i + 8] = b2[i];
+  }
 }
 
-void join(int *resp, int *x){
-	int i;
+int soma(int *b1, int *b2, int *resp, int vaiUm) {
+  int i;
 
-	for(i=0; i<16; i++)
-		resp[i] = x[i];	
+  for (i = 15; i >= 0; i--) {
+
+    int soma = vaiUm + b1[i] + b2[i];
+
+    if (soma == 0) {
+      resp[i] = 0;
+      vaiUm = 0;
+    } else if (soma == 1) {
+      resp[i] = 1;
+      vaiUm = 0;
+    } else if (soma == 2) {
+      resp[i] = 0;
+      vaiUm = 1;
+    } else if (soma == 3) {
+      resp[i] = 1;
+      vaiUm = 1;
+    }
+  }
+
+  return vaiUm;
 }
 
-void comp1(int *resp, int *check, int goOne){
-	int v1[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
-	
-	if(goOne == 1)
-		sum(resp, v1, check);
-	else
-		join(check, resp);
+int checksum_verify(int bytes_normalizados[30000][8], int *check_concat,
+                    int *prot, int campos, int data[30000][8], int index_data) {
+  int i;
+  int adControl[16];
+  int check_aux[16];
+  int resp[16];
+  int vaiUm = 0;
+  int aux_sum[16];
+  int concat[16];
+  int sum_check_concat[16], aux = 1;
+
+  concatenar(bytes_normalizados[0], bytes_normalizados[1], adControl);
+
+  vaiUm = soma(adControl, prot, resp, vaiUm);
+
+  for (i = 1; i < index_data; i = i + 2) {
+    concatenar(data[i - 1], data[i], concat);
+    vaiUm = soma(resp, concat, resp, vaiUm);
+  }
+
+  soma(resp, check_concat, sum_check_concat, vaiUm);
+
+  for (i = 0; i < 16; i++) {
+    if (sum_check_concat[i] != 1)
+      aux = 0;
+  }
+
+  return aux;
 }
 
-int check(int entrada[CARAC][8], int *ccks, int *prot, int campos, int semEscape[CARAC][8], int index_semEscape){
-	int i, adControl[16], check_aux[16], resp[16], goOne, x[16], concat[16], sum_ccks[16], aux=0;
-	
-	concatenation(entrada[0], entrada[1], adControl);
-	
-	goOne = sum(adControl, prot, check_aux);
-	
-	comp1(check_aux, resp, goOne);
-	
-	for(i=0; i<index_semEscape; i=i+2){
-		concatenation(semEscape[i], semEscape[i+1], concat);
-		goOne = sum(resp, concat, x);
-		comp1(x, resp, goOne);
-	}
-	
-	goOne = sum(resp, ccks, sum_ccks);
-	
-	for(i=0; i<16; i++){
-		if(sum_ccks[i] != 1)
-			aux = 1;
-	}
-	
-	if(aux == 0)
-		return 1;
-	else
-		return 0;
+int equal(int *b1, int *b2) {
+  int isEqual = 1;
+
+  int i = 0;
+  while (i < 8) {
+    if (b1[i] != b2[i])
+      isEqual = 0;
+    i++;
+  }
+
+  return isEqual;
 }
 
-int equal(int *vec1, int *vec2){
-	int aux=0, j=0;
-	
-	while(j<8){
-		if(vec1[j] != vec2[j])
-			aux = 1;
-		j++;
-	}
-	if(aux == 0)
-		return 1;
-	else 
-		return 0;
+void normalizar_input(int bytes_normalizados[30][6251][8], int *palavras,
+                      int input[30000][8], int qtd_bytes, int *campos) {
+  int flag[8] = {0, 1, 1, 1, 1, 1, 1, 0};
+  int escape[8] = {0, 1, 1, 1, 1, 1, 0, 1};
+  int in, j, i;
+
+  (*palavras)++;
+  campos[*palavras] = 0;
+  in = 1;
+
+  for (i = 1; i < qtd_bytes; i++) {
+    if (equal(input[i - 1], escape) == 1 || equal(input[i], flag) == 0) {
+      in = 0;
+      for (j = 0; j < 8; j++) {
+        bytes_normalizados[*palavras][campos[*palavras]][j] = input[i][j];
+      }
+      campos[*palavras]++;
+    } else {
+      if (in == 0) {
+        (*palavras)++;
+        campos[*palavras] = 0;
+        in = 1;
+      }
+    }
+  }
 }
 
-void charToworld(int entrada[CARAC][8], int campo, int semEscape[CARAC][8], int *index_semEscape){
-	int escape[8] = {0,1,1,1,1,1,0,1};
-	int i, j=0;
-	
-	for(i=4; i<campo-2; i++){
-		if(equal(entrada[i], escape) == 0){
-			j=0;
-			while(j<8){
-				semEscape[*index_semEscape][j] = entrada[i][j];
-				j++;
-			}
-			(*index_semEscape)++;
-		}else{
-			if(equal(entrada[i-1], escape) == 1){
-				j=0;
-				while(j<8){
-					semEscape[*index_semEscape][j] = entrada[i][j];
-					j++;
-				}
-				(*index_semEscape)++;
-			}
-		}
-		
-	}	
-}
+void print(int bytes_normalizados[30000][8], int campos, int index,
+           int palavras) {
 
-void receptor(int entrada[CARAC][8], int campos, int index_out, int words){
-	
-	char protocol[4], checksum[4];
-	int i, verify, index_semEscape=0, adress, control, ccks[16], prot[16], payload[CARAC], semEscape[CARAC][8];
+  char *protocol, *checksum;
+  int i, j, verify_checksum, index_data = 0, address, control;
+  int check_concat[16], prot[16], data[30000][8];
+  int escape[8] = {0, 1, 1, 1, 1, 1, 0, 1};
 
-	adress = binTodec(entrada[0]);
-	
-	control = binTodec(entrada[1]);
-	
-	concatenation(entrada[2], entrada[3], prot);
-	
-	binTohexa(prot, protocol);
-	
-	charToworld(entrada, campos, semEscape, &index_semEscape);
-	
-	for(i=0; i<index_semEscape; i++)
-		payload[i] = binTodec(semEscape[i]);
-	
-	concatenation(entrada[campos-2], entrada[campos-1], ccks);
-	
-	binTohexa(ccks, checksum);
-	
-	if(index_semEscape%2 != 0){
-		for(i=0; i<8; i++)
-			semEscape[index_semEscape][i] = 0;
-		index_semEscape++;
-	}
-	
-	verify = check(entrada, ccks, prot, campos, semEscape, index_semEscape);
-	
-	printf("| PPP Frame %d control fields |\n\n", index_out);
-	printf("Address: %d\n\n", adress);
-	printf("Control: %d\n\n", control);
-	printf("Protocol: ");
-	for(i=0; i<4; i++)
-		printf("%c", protocol[i]);
-		
-	printf("\n\nChecksum: ");
-	for(i=0; i<4; i++)
-		printf("%c", checksum[i]);
-	printf(" (Binary ");
-	for(i=0; i<16; i++)	
-		printf("%d", ccks[i]);
-	printf(")");
-	printf("\n\nData: ");
-	
-	for(i=0; i<index_semEscape; i++)
-		printf("%c", payload[i]);	
-		
-	
-	if(index_out == words-1){
-		if(verify == 1)
-			printf("\n\nData integrity: ok.\n");
-		else
-			printf("\n\nData integrity: not ok.\n");
-	}else{
-		if(verify == 1)
-			printf("\n\nData integrity: ok.\n\n");
-		else
-			printf("\n\nData integrity: not ok.\n\n");
-	}
-	
-}
+  printf("| PPP Frame %d control fields |\n\n", index);
 
-void preProcessing(int input_with_preproc[WORDS][6251][8], int *words, int input[CARAC][8], int bytes, int *campos){
-	int flag[8] = {0,1,1,1,1,1,1,0};
-	int escape[8] = {0,1,1,1,1,1,0,1};
-	int entrou, j, i;
-	
-	(*words)++;
-	campos[*words] = 0;
-	entrou = 1;
-	
-	for(i=1; i<bytes; i++){
-		if(equal(input[i-1], escape) == 1 || equal(input[i], flag) == 0){
-			entrou = 0;
-			for(j=0; j<8; j++){
-				input_with_preproc[*words][campos[*words]][j] = input[i][j];
-			}
-			campos[*words] = campos[*words] + 1;
-		}else{
-			if(entrou == 0){
-				(*words)++;
-				campos[*words] = 0;
-				entrou = 1;
-			}
-		}
-	}	
-	
-}
-int main(){
-	int i, j, n, bytes, input[CARAC][8], input_with_preproc[WORDS][6251][8], words=-1, campos[100];
-	char s[LEN_BIN];
-	
-	scanf("%s", s);
-	n = strlen(s);
-	
-	bytes = n/8;
-	
-	for(i=0; i<bytes; i++){
-		for(j=0; j<8; j++){
-			if(s[(i*8)+j] == '0')
-				input[i][j] = 0;
-			else
-				input[i][j] = 1;
-		}
-	}
-	
-	preProcessing(input_with_preproc, &words, input, bytes, campos);
-	
-	for(i=0; i<words; i++){
-		receptor(input_with_preproc[i], campos[i], i, words);
-	}
-	
-	return 0;
+  address = binToDec(bytes_normalizados[0], 8);
+  control = binToDec(bytes_normalizados[1], 8);
+
+  concatenar(bytes_normalizados[2], bytes_normalizados[3], prot);
+  protocol = (char *)malloc(sizeof(char *) * 4);
+  binToHex(prot, protocol);
+
+  printf("Address: %d\n\nControl: %d\n\n", address, control);
+  printf("Protocol: ");
+  for (i = 0; i < 4; i++)
+    printf("%c", protocol[i]);
+
+  concatenar(bytes_normalizados[campos - 2], bytes_normalizados[campos - 1],
+             check_concat);
+  checksum = (char *)malloc(sizeof(char *) * 16);
+  binToHex(check_concat, checksum);
+
+  printf("\n\nChecksum: ");
+  for (i = 0; i < 4; i++)
+    printf("%c", checksum[i]);
+  printf(" (Binary ");
+  for (i = 0; i < 16; i++)
+    printf("%d", check_concat[i]);
+  printf(")");
+
+  /* Filtrando data */
+  for (i = 4; i < campos - 2; i++) {
+    if (equal(bytes_normalizados[i], escape) == 0) {
+      j = 0;
+      while (j < 8) {
+        data[index_data][j] = bytes_normalizados[i][j];
+        j++;
+      }
+      index_data++;
+    } else if (equal(bytes_normalizados[i - 1], escape) == 1) {
+      j = 0;
+      while (j < 8) {
+        data[index_data][j] = bytes_normalizados[i][j];
+        j++;
+      }
+      index_data++;
+    }
+  }
+
+  if (index_data % 2 != 0) {
+    for (i = 0; i < 8; i++)
+      data[index_data][i] = 0;
+    index_data++;
+  }
+
+  printf("\n\nData: ");
+  for (i = 0; i < index_data; i++) {
+    printf("%c", binToDec(data[i], 8));
+  }
+
+  verify_checksum = checksum_verify(bytes_normalizados, check_concat, prot,
+                                    campos, data, index_data);
+
+  if (verify_checksum == 1)
+    printf("\n\nData integrity: ok.\n");
+  else
+    printf("\n\nData integrity: not ok.\n");
 }
